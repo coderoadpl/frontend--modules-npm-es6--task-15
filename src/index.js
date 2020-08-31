8d1606b84de3b97448b0d7fa6619e22edb3457d2
Mateusz Choma <kontakt@coderoad.pl>
2020-08-31 08:23:14 +0200

    Source maps added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log('Hello Webpack!')
+
+class Greeter {
+
+    constructor() {
+        throw new Error()
+    }
+
+}
+
+const greeter1 = new Greeter()
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const path = require('path')
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
+    devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.[hash].js',
